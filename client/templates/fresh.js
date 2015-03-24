8a09e3f7c272e6c7b9f26823aacf6f6a34e4e25c
angga <sirin@karuniaindah.net>
2015-03-24 15:33:45 +0700

    ok

--- client/templates/fresh.js
+++ client/templates/fresh.js
@@ -1,61 +1,28 @@
-Template.fresh.rendered = function(){
-  $('#searchInput').val('');
-
-  // if(($("#data").height() >= $("div.ui.sticky.data-left").height()) && ($("body").width() > 979)){
-    $('.ui.sticky.data-left').sticky({
-      offset  : 50,
-      context : '#data'
+Template.fresh.events({
+  'click [data-action=shareApp]': function (event, template) {
+    IonActionSheet.show({
+      titleText: 'Share on :',
+      buttons: [
+        { text: 'Facebook <i class="icon ion-share"></i>' },
+        { text: 'Twitter <i class="icon ion-arrow-move"></i>' },
+        { text: 'Google + <i class="icon ion-arrow-move"></i>' },
+      ],
+      buttonClicked: function(index) {
+        if (index === 0) {
+          console.log('Facebook!');
+        }
+        if (index === 1) {
+          console.log('Twitter!');
+        }
+        if (index === 2) {
+          console.log('Google!');
+        }
+        return true;
+      },
+      destructiveButtonClicked: function() {
+        console.log('Destructive Action!');
+        return true;
+      }
     });
-  // }
-
-  $(document).on( 'scroll', function(){
-    if ($(window).scrollTop() > 100) {
-      $('.scroll-top-wrapper').addClass('show');
-    } else {
-      $('.scroll-top-wrapper').removeClass('show');
-    }
-  });
-  $('.scroll-top-wrapper').on('click', scrollToTop);
-
-};
-
-var libhtml = function() {
-};
-
-var libsticky = function() {
-  setInterval(function () { autoloadpage(); }, 1000);
-  function autoloadpage() {
-    if($("body").width() > 979){
-      $('.datarun').Stickyfill();
-    }
-    $('html body').css("height", "auto");
   }
-};
-
-Template.testpost.rendered = function(){
-  Meteor.Loader.loadJs("/sticky/html5shiv.js", libhtml);
-  Meteor.Loader.loadJs("/sticky/stickyfill.js", libsticky);
-};
-
-Template.testpost.helpers({
-  images: function () {
-    return Images.find(); // Where Images is an FS.Collection instance
-  },
-  posts: function () {
-    return Posts.find(); // Where Images is an FS.Collection instance
-  },
-  postImage: function () {
-    return Images.findOne(this.imageId);; // Where Images is an FS.Collection instance
-  },
-  testImage: function () {
-    return Images.find();; // Where Images is an FS.Collection instance
-  },
 });
-
-function scrollToTop() {
-  verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-  element = $('body');
-  offset = element.offset();
-  offsetTop = offset.top;
-  $('html, body').animate({scrollTop: offsetTop}, 100, 'linear');
-}
--- client/templates/trending.js
+++ client/templates/trending.js
@@ -1,102 +1,28 @@
-Template.trending.rendered = function(){
-  $('#searchInput').val('');
-
-  // if(($("#data").height() >= $("div.ui.sticky.data-left").height()) && ($("body").width() > 979)){
-    $('.ui.sticky.data-left').sticky({
-      offset  : 50,
-      context : '#data'
-    });
-  // }
-
-  $(document).on( 'scroll', function(){
-    if ($(window).scrollTop() > 100) {
-      $('.scroll-top-wrapper').addClass('show');
-    } else {
-      $('.scroll-top-wrapper').removeClass('show');
-    }
-  });
-  $('.scroll-top-wrapper').on('click', scrollToTop);
-
-};
-
-Template.trending.helpers({
-  moreResults: function () {
-    return !(Posts.find().count() < Session.get("itemsLimit"));
-  }
-});
-
-var libhtml = function() {
-};
-
-var libsticky = function() {
-    // $('.datarun').Stickyfill();
-    setInterval(function () { autoloadpage(); }, 3000);
-    function autoloadpage() {
-      if($("body").width() > 979){
-        $('.datarun').Stickyfill();
+Template.trending.events({
+  'click [data-action=shareApp]': function (event, template) {
+    IonActionSheet.show({
+      titleText: 'Share on :',
+      buttons: [
+        { text: 'Facebook <i class="icon ion-share"></i>' },
+        { text: 'Twitter <i class="icon ion-arrow-move"></i>' },
+        { text: 'Google + <i class="icon ion-arrow-move"></i>' },
+      ],
+      buttonClicked: function(index) {
+        if (index === 0) {
+          console.log('Facebook!');
+        }
+        if (index === 1) {
+          console.log('Twitter!');
+        }
+        if (index === 2) {
+          console.log('Google!');
+        }
+        return true;
+      },
+      destructiveButtonClicked: function() {
+        console.log('Destructive Action!');
+        return true;
       }
-      $('html body').css("height", "auto");
-    }
-
-};
-
-Template.testpost.rendered = function(){
-  Meteor.Loader.loadJs("/sticky/html5shiv.js", libhtml);
-  Meteor.Loader.loadJs("/sticky/stickyfill.js", libsticky);
-
-  // $('.download-button').on( 'click', function(){
-  //   console.log($(this).attr('id'));
-  // });
-};
-
-Template.testpost.events({
-  'click .download-button' :function (e){
-    SaveToDisk(e.currentTarget.id, 'OpoKuwi');
+    });
   }
 });
-
-Template.testpost.helpers({
-  images: function () {
-    return Images.find(); // Where Images is an FS.Collection instance
-  },
-  posts: function () {
-    return Posts.find(); // Where Images is an FS.Collection instance
-  },
-  postImage: function () {
-    return Images.findOne(this.imageId);; // Where Images is an FS.Collection instance
-  },
-  testImage: function () {
-    return Images.find();; // Where Images is an FS.Collection instance
-  },
-});
-
-function scrollToTop() {
-  verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-  element = $('body');
-  offset = element.offset();
-  offsetTop = offset.top;
-  $('html, body').animate({scrollTop: offsetTop}, 100, 'linear');
-}
-
-function SaveToDisk(fileURL, fileName) {
-    // for non-IE
-    if (!window.ActiveXObject) {
-        var save = document.createElement('a');
-        save.href = fileURL;
-        save.target = '_blank';
-        save.download = fileName || 'unknown';
-
-        var event = document.createEvent('Event');
-        event.initEvent('click', true, true);
-        save.dispatchEvent(event);
-        (window.URL || window.webkitURL).revokeObjectURL(save.href);
-    }
-
-    // for IE
-    else if ( !! window.ActiveXObject && document.execCommand)     {
-        var _window = window.open(fileURL, '_blank');
-        _window.document.close();
-        _window.document.execCommand('SaveAs', true, fileName || fileURL)
-        _window.close();
-    }
-}
